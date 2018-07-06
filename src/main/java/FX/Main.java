2ebf0fbc93b2582255529923da2a79435d37c815
elijah.b.i.p <elijah.b.i.p@gmail.com>
2018-07-06 05:09:01 +0400

    Need to fix
    Signed-off-by: elijah.b.i.p <elijah.b.i.p@gmail.com>

--- src/main/java/FX/Main.java
+++ src/main/java/FX/Main.java
@@ -1,16 +1,5 @@
 package FX;
 
-import com.vk.api.sdk.client.TransportClient;
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.objects.users.UserXtrCounters;
-import com.vk.api.sdk.objects.wall.WallPostFull;
-import com.vk.api.sdk.objects.wall.responses.GetResponse;
-import com.vk.api.sdk.queries.users.UserField;
-import com.vk.api.sdk.queries.wall.WallGetFilter;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -21,12 +10,8 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import vk_init.ClientVK;
-import vk_init.TotalInformationVO;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 
 public class Main extends Application {
@@ -46,40 +31,37 @@ public class Main extends Application {
             "&revoke=1"+
             "&v=5.59";
 
+    ClientVK clientVK;
 
     public static void main(String[] args){
         launch(args);
     }
 
     private void auth(String newValue) {
-        ClientVK clientVK = new ClientVK(newValue);
-        TotalInformationVO totalInformationVO = clientVK.getTotalInformation("asja_1111");
-
-
-       System.out.println((totalInformationVO.getUser()).getFirstName()
-               + " "
-               +  totalInformationVO.getUser().getLastName());
-        System.out.println(totalInformationVO.getCountComments_Wall());
-        System.out.println(totalInformationVO.getCountLikes_Wall());
+        this.clientVK = new ClientVK(newValue);
+        Controller.setClient(this.clientVK);
     }
 
-    private void setHeadScene(Stage primaryStage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("12.fxml"));
+    private void setSearchScene(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Main form");
-        primaryStage.setScene(new Scene(parent,600,400));
+        primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        setHeadScene(primaryStage);
+
         WebView webView = new WebView();
         WebEngine webEngine = webView.getEngine();
         webEngine.load(VK_AUTH);
 
         primaryStage.setScene(new Scene(webView));
         primaryStage.show();
+
+
+
         webEngine.locationProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -87,7 +69,11 @@ public class Main extends Application {
                     System.out.println(newValue);
                     primaryStage.close();
                     auth(newValue);
-
+                    try {
+                        setSearchScene(primaryStage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
